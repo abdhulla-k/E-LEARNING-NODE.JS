3fc859b865841f1bd7b6dc66f78b2b233dff6df3
abdhulla-k <abdhullak149@gmail.com>
2022-12-28 22:58:33 +0530

    congigured core and connected front end and back end

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 // import express
 const express = require('express');
 
+// import cors
+const cors = require('cors');
+
 // create the instance of expresslibrary
 const app = express();
+
+// configuring cors
+const corsOptions = {
+    origin: 'http://localhost:9000'
+}
+app.use(cors(corsOptions));
 
 // import routers
 const userRoutes = require('./routes/home');
