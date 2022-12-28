4c785f59b5e14d3b925c7e0df0ca02a3add396c7
abdhulla-k <abdhullak149@gmail.com>
2022-12-28 23:37:22 +0530

    connected with mongodb, used jwt and dontenv

--- app.js
+++ app.js
@@ -4,8 +4,20 @@ const express = require('express');
 // import cors
 const cors = require('cors');
 
+// import dotenv
+const dotenv = require('dotenv');
+
+// import jwt
+const jwt = require('jsonwebtoken');
+
+// import mongoose
+const mongoose = require('mongoose');
+
 // create the instance of expresslibrary
 const app = express();
+
+// Set up Global configuration access for dotenv
+dotenv.config();
 
 // configuring cors
 const corsOptions = {
@@ -19,5 +31,12 @@ const userRoutes = require('./routes/home');
 // to user routes
 app.use('/', userRoutes);
 
-// make it possible to listen
-app.listen(3000);
+// connect with database
+mongoose.connect("mongodb://localhost:27017/newApplication").then(data => {
+    console.log("connected");
+
+    // make it listenable
+    app.listen(3000);
+}).catch(err => {
+    console.log(err);
+})
--- controllers/home.js
+++ controllers/home.js
@@ -1,3 +1,14 @@
+// import jwt
+const jwt = require('jsonwebtoken');
+
+let jwtSecretKey = process.env.JWT_SECRET_KEY;
+
 exports.homePage = (req, res, next) => {
+  let data = {
+    time: Date(),
+    userId: 12
+  }
+  const token = jwt.sign(data, jwtSecretKey);
+  console.log(token);
   res.json({ title: 'Home page' });
 }
