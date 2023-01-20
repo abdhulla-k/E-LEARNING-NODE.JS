4d91d2c3c8f9eec1bef8c68f82fa22a0ee86275c
abdhulla-k <abdhullak149@gmail.com>
2023-01-20 23:14:50 +0530

    solved some eslint errors

--- controllers/home.js
+++ controllers/home.js
@@ -1,14 +1,14 @@
 // import jwt
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken')
 
-let jwtSecretKey = process.env.JWT_SECRET_KEY;
+const jwtSecretKey = process.env.JWT_SECRET_KEY
 
 exports.homePage = (req, res, next) => {
-  let data = {
+  const data = {
     time: Date(),
     userId: 12
   }
-  const token = jwt.sign(data, jwtSecretKey);
-  console.log(token);
-  res.json({ title: 'Home page' });
+  const token = jwt.sign(data, jwtSecretKey)
+  console.log(token)
+  res.json({ title: 'Home page' })
 }
