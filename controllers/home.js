77a6d06cd86744dacf6b59374498d51942c9339c
abdhulla-k <abdhullak149@gmail.com>
2023-02-28 18:20:12 +0530

    created search functionality

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,6 @@
 // import jwt
 const jwt = require('jsonwebtoken')
+const Courses = require('../models/courses')
 
 const jwtSecretKey = process.env.JWT_SECRET_KEY
 
@@ -11,4 +12,18 @@ exports.homePage = (req, res, next) => {
   const token = jwt.sign(data, jwtSecretKey)
   console.log(token)
   res.json({ title: 'Home page' })
+}
+
+// to search courses
+// /search
+exports.search = async (req, res, next) => {
+  try {
+    const pattern = `${req.query.searchData}`
+    // Search for documents based on a regular expression
+    const query = { title: { $regex: pattern } }
+    const cursor = await Courses.find(query)
+    res.status(200).json({ status: true, data: cursor })
+  } catch {
+    res.status(500).json({ status: false, message: 'error found while searching' })
+  }
 }
--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,15 @@
 // import express and create an instance
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 // home controllers. It is for all users
-const userControllers = require('../controllers/home');
+const homeController = require('../controllers/home')
 
 // for home page. empty url
-router.get('/', userControllers.homePage);
+router.get('/', homeController.homePage)
+
+// to search courses
+router.get('/courses/search', homeController.search)
 
 // export the router
-module.exports = router;
+module.exports = router
